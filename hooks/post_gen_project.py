8ce3e242a3ff5a19dc577bf6a3b9c221feed431e
pepito <pepito@pepito.com>
2024-03-14 11:55:31 -0300

    un poco de 5 Useful F-String Tricks In Python

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,9 @@ import subprocess
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
+print(f"{MESSAGE_COLOR:_>100}Almost done!")
+print(f"{MESSAGE_COLOR:#<100}Almost done!")
+print(f"{MESSAGE_COLOR:|^100}Almost done!")
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
 
